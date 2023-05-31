7ad483f93effc37f3f83d1523581ab9a3d2d26e4
Luke Vo <datvm@outlook.com>
2023-06-01 01:33:38 +0700

    Added a hosting web project to demonstrate the CORS error.

--- BlazorHasOwnPropertyBugDemo/wwwroot/BlazorHasOwnPropertyBugDemo.lib.module.js
+++ BlazorHasOwnPropertyBugDemo/wwwroot/BlazorHasOwnPropertyBugDemo.lib.module.js
@@ -1,21 +1,7 @@
 globalThis.wasmInterop = new class {
 
     getOpener() {
-        // Fake Window object, for cross-origin opener, most operations will throw an error
-        // return window.opener;
-
-        const proxy = new Proxy({
-            postMessage: (msg) => {
-                alert(msg);
-            }
-        }, {
-            getOwnPropertyDescriptor(target, p) {
-                console.log(p + " descriptor requested");
-                throw new Error("Permission denied");
-            }
-        });
-
-        return globalThis.debugObj = proxy;
+        return globalThis.parent;
     }
 
     sendReadyMessage(window, op) {
